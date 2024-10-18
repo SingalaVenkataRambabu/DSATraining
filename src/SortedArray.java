830a312a4606b2683609a06aa20d20bd80fb388d
Singala.Rambabu <srambabu200922@gmail.com>
2024-10-18 14:32:43 +0530

    Finding minimum number in nonsorted array

---
+++ src/SortedArray.java
@@ -0,0 +1,34 @@
+public class SortedArray {
+    public static void main(String[] args)
+    {
+        int[] arr1 = {5, 6, 1, 2, 3, 4};
+        int minNumber = findMinimumNumber(arr1);
+        System.out.println("The minimum number is: " + minNumber);
+    }
+    private static int findMinimumNumber(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        if (nums[left] < nums[right]) {
+            return nums[left];
+        }
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+             if (mid > 0 && nums[mid] < nums[mid - 1]) {
+                return nums[mid];
+            }
+            if (mid < nums.length - 1 && nums[mid] > nums[mid + 1]) {
+                return nums[mid + 1];
+            }
+            if (nums[mid] >= nums[left]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
+
+
+
