44853dcaa6188ed9b912fbe27f2960ea3d694953
Singala.Rambabu <srambabu200922@gmail.com>
2024-10-29 15:32:51 +0530

    StairCase Program

---
+++ src/CaseStair.java
@@ -0,0 +1,25 @@
+public class CaseStair {
+    public static void main(String[] args) {
+
+        int n=5;
+        System.out.println("StairCase program:: "+countWays(n));
+
+    }
+    public static int countWays(int n) {
+        if(n<=1) {
+            return 1;
+        }
+
+        int previousStep = 1;
+        int currentStep = 2;
+
+        for(int i=3;i<=n;i++) {
+            int nextStep=previousStep+currentStep;
+            previousStep=currentStep;
+            currentStep = nextStep;
+
+        }
+        return currentStep;
+    }
+
+}
